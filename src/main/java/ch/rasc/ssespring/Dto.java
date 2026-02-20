25c2b9746ee2c5bed53aa825b7296efda38cb371
Ralph Schaer <ralphschaer@gmail.com>
2026-02-20 10:04:34 +0100

    upgrade

--- src/main/java/ch/rasc/ssespring/Dto.java
+++ src/main/java/ch/rasc/ssespring/Dto.java
@@ -1,20 +1,4 @@
 package ch.rasc.ssespring;
 
-public class Dto {
-	private int i;
-	private String s;
-	public int getI() {
-		return this.i;
-	}
-	public void setI(int i) {
-		this.i = i;
-	}
-	public String getS() {
-		return this.s;
-	}
-	public void setS(String s) {
-		this.s = s;
-	}
-
-
+public record Dto(int i, String s) {
 }
